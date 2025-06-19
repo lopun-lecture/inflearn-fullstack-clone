0d423495a7c9c215d01ee890e2fca4f91d94943f
Lopun Lecture <lopun.lecture@gmail.com>
2025-06-19 16:15:35 +0900

    [BE] 질문 작성·조회·답변 API

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,6 +10,8 @@ import { SectionsModule } from './sections/sections.module';
 import { CategoriesModule } from './categories/categories.module';
 import { MediaModule } from './media/media.module';
 import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
+import { QuestionsModule } from './questions/questions.module';
 
 @Module({
   imports: [
@@ -22,6 +24,8 @@ import { UsersModule } from './users/users.module';
     CategoriesModule,
     MediaModule,
     UsersModule,
+    CommentsModule,
+    QuestionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
