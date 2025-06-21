4ac0a0b98ecddb93376ee649fb62a14f7af4fd1e
Lopun Lecture <lopun.lecture@gmail.com>
2025-06-21 23:53:16 +0900

    [BE] NestJS Cron(Scheduler)로 일간 통계 배치 작업 구현

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,6 +14,7 @@ import { CommentsModule } from './comments/comments.module';
 import { QuestionsModule } from './questions/questions.module';
 import { CartsModule } from './carts/carts.module';
 import { PaymentsModule } from './payments/payments.module';
+import { BatchModule } from './batch/batch.module';
 
 @Module({
   imports: [
@@ -30,6 +31,7 @@ import { PaymentsModule } from './payments/payments.module';
     QuestionsModule,
     CartsModule,
     PaymentsModule,
+    BatchModule,
   ],
   controllers: [AppController],
   providers: [AppService],
